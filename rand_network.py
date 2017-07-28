c3e8db02d710f2416702428cca84816466e9c54d
deniskup <denis.kuperberg@gmail.com>
2017-07-28 15:16:31 +0200

    random neural network

--- rand_network.py
+++ rand_network.py
@@ -4,7 +4,7 @@
 #import modules
 from PIL import Image
 from random import random
-
+from math import *
 
 # 2 inputs for coordinates x,y
 # 3 outputs for R,G,B
@@ -32,18 +32,48 @@ for i in range(inter):
  	weight2.append(wl)
  
 
-def changepix(pix):
-	return pix
+
+#activation function
+#let's try with the sigmoid
+def activ(x):
+	return 1/(1+exp(-x))
+
+#run the neural network on inputs [x,y]
+def run(inp):
+	#compute values of intermediate neurons
+	intvalues=[]
+	for j in range(inter):
+		val=0
+		for i in range(inputs):
+			val+=inp[i]*weight1[i][j]
+		#apply activation function
+		val=activ(val)
+		intvalues.append(val)
+	#now compute output values
+	outvalues=[]
+	for j in range(outputs):
+		val=0
+		for i in range(inter):
+			val+=intvalues[i]*weight2[i][j]
+		#apply activation function
+		val=activ(val)
+		#scaling to obtain an integer between 0 and 255
+		color=int(val*256)
+		outvalues.append(color)
+		
+	#return the RGB triplet
+	print outvalues
+	return (outvalues[0],outvalues[1],outvalues[2])
 
 
 im = Image.open("imageGO.jpg")
 (width,height)=im.size
-res=Image.new("RGB",(width,height));
+res=Image.new("RGB",(width,height))
 pix1=im.load()
 pix2=res.load()
 for x in range(width):
 	for y in range(height):
-		pix2[x,y]=changepix(pix1[x,y])
+		pix2[x,y]=run([x,y])
 
 res.show()
 
