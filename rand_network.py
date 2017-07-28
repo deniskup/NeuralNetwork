79f3833be0cab4e27e9ea1c17a60b5dab47557c2
deniskup <denis.kuperberg@gmail.com>
2017-07-28 15:46:30 +0200

    bigger weights

--- rand_network.py
+++ rand_network.py
@@ -22,58 +22,70 @@ weight2=[] #two-dimensional array for inter to output
 for i in range(inputs):
  	wl=[]
  	for j in range(inter):
- 		wl.append(random())
+ 		wl.append(random()*10-5)
  	weight1.append(wl)
 #then weight2
 for i in range(inter):
  	wl=[]
  	for j in range(outputs):
- 		wl.append(random())
+ 		wl.append(random()*10-5)
  	weight2.append(wl)
  
 
 
-#activation function
-#let's try with the sigmoid
+#activation functions
+#identity
+def activid(x):
+	return x
+
+#sigmoid
 def activ(x):
 	return 1/(1+exp(-x))
 
+
+#load image to obtain size
+im = Image.open("imageGO.jpg")
+(width,height)=im.size
+pixin=im.load()
+
 #run the neural network on inputs [x,y]
 def run(inp):
+	#normalize the input to values between 0 and 1
+	normx=inp[0]/(width*1.0)
+	normy=inp[1]/(height*1.0)
+	inval=[normx,normy]
 	#compute values of intermediate neurons
 	intvalues=[]
 	for j in range(inter):
-		val=0
+		val=0.
 		for i in range(inputs):
-			val+=inp[i]*weight1[i][j]
+			val+=inval[i]*weight1[i][j]
 		#apply activation function
 		val=activ(val)
 		intvalues.append(val)
 	#now compute output values
 	outvalues=[]
 	for j in range(outputs):
-		val=0
+		val=0.
 		for i in range(inter):
 			val+=intvalues[i]*weight2[i][j]
 		#apply activation function
 		val=activ(val)
 		#scaling to obtain an integer between 0 and 255
-		color=int(val*256)
+		color=int(floor(val*256))
 		outvalues.append(color)
 		
 	#return the RGB triplet
-	print outvalues
+	#print outvalues
 	return (outvalues[0],outvalues[1],outvalues[2])
 
 
-im = Image.open("imageGO.jpg")
-(width,height)=im.size
+
 res=Image.new("RGB",(width,height))
-pix1=im.load()
-pix2=res.load()
+pixout=res.load()
 for x in range(width):
 	for y in range(height):
-		pix2[x,y]=run([x,y])
+		pixout[x,y]=run([x,y])
 
 res.show()
 
